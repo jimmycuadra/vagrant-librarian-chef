3edcd097fad3acfefa029065cb8ddc9b8d69c54e
Felix Schäfer <felix@fachschaften.org>
2013-05-14 13:53:45 +0200

    Only run Librarian when a Cheffile exists in the Vagrant root #1

--- lib/vagrant-librarian-chef/action/librarian_chef.rb
+++ lib/vagrant-librarian-chef/action/librarian_chef.rb
@@ -10,11 +10,14 @@ module VagrantPlugins
         end
 
         def call(env)
-          env[:ui].info "Installing Chef cookbooks with Librarian-Chef..."
-          environment = Librarian::Chef::Environment.new
-          Librarian::Action::Ensure.new(environment).run
-          Librarian::Action::Resolve.new(environment).run
-          Librarian::Action::Install.new(environment).run
+          # look for a Cheffile in the Vagrant root_path
+          if FileTest.exist?(env[:root_path] + "Cheffile")
+            env[:ui].info "Installing Chef cookbooks with Librarian-Chef..."
+            environment = Librarian::Chef::Environment.new
+            Librarian::Action::Ensure.new(environment).run
+            Librarian::Action::Resolve.new(environment).run
+            Librarian::Action::Install.new(environment).run
+          end
           @app.call(env)
         end
       end
